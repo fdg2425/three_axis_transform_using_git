f17287a033eeea87e1cf94cd76d45885bb5f5bfa
gs <gs@xyz.de>
2025-01-26 14:41:49 +0100

    widget AxisSlider in an own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
+
+import 'axis_slider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -120,37 +121,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-    );
-  }
-}
-
-class AxisSlider extends StatelessWidget {
-  const AxisSlider({
-    super.key,
-    required this.title,
-    required this.color,
-    required this.angle,
-    required this.callback,
-  });
-
-  final String title;
-  final Color color;
-  final double angle;
-  final Function(double value) callback;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(title, style: TextStyle(color: color)),
-        Slider(
-          value: angle,
-          min: -2 * pi,
-          max: 2 * pi,
-          onChanged: callback,
-        ),
-      ],
     );
   }
 }
